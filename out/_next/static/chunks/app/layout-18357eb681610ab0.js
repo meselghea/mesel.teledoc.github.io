2eef44ed1a5a8999dfc8dc54f1d2b84698f0e8ea
meselghea <122159476+meselghea@users.noreply.github.com>
2023-10-23 22:34:05 +0700

    Deploy

---
+++ out/_next/static/chunks/app/layout-18357eb681610ab0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3124:function(n,a,e){Promise.resolve().then(e.t.bind(e,3925,23))},3925:function(n){n.exports={style:{fontFamily:"'__Inter_aa2d5c', '__Inter_Fallback_aa2d5c'",fontStyle:"normal"},className:"__className_aa2d5c"}}},function(n){n.O(0,[971,472,744],function(){return n(n.s=3124)}),_N_E=n.O()}]);
