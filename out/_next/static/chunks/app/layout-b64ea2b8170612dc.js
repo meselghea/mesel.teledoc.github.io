a0258aa7bd69f97c3f76c26934e2f8884481a30b
meselghea <122159476+meselghea@users.noreply.github.com>
2023-10-24 20:12:15 +0700

    clear all

--- out/_next/static/chunks/app/layout-b64ea2b8170612dc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3124:function(n,e,_){Promise.resolve().then(_.t.bind(_,3925,23))},3925:function(n){n.exports={style:{fontFamily:"'__Inter_0d3b3f', '__Inter_Fallback_0d3b3f'",fontStyle:"normal"},className:"__className_0d3b3f"}}},function(n){n.O(0,[971,472,744],function(){return n(n.s=3124)}),_N_E=n.O()}]);
